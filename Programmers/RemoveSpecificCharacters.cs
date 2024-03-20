e4bb4fe4745155d7afa874f8e93bbd100086db71
MongLick <qkjm4836@gmail.com>
2024-03-20 17:01:30 +0900

    feat : 특정 문자 제거하기

---
+++ Programmers/RemoveSpecificCharacters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+public class Solution
+{
+    public string solution(string my_string, string letter)
+    {
+        string answer = "";
+
+        string[] SplitResult = my_string.Split(letter);
+
+        for (int i = 0; i < SplitResult.Length; i++)
+        {
+            answer += SplitResult[i];
+        }
+        return answer;
+    }
+}
