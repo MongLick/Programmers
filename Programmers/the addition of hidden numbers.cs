5cf2349773c0efc86363030627652d22d6ec27aa
MongLick <qkjm4836@gmail.com>
2024-04-03 18:10:21 +0900

    feat : 숨어있는 숫자의 덧셈(1)

---
+++ Programmers/the addition of hidden numbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Solution
+{
+	public int solution(string my_string)
+	{
+		int answer = 0;
+
+		string temp = Regex.Replace(my_string, @"\D", "");
+		for (int i = 0; i < temp.Length; i++)
+		{
+			answer += int.Parse(temp[i].ToString());
+		}
+
+		return answer;
+	}
+}
